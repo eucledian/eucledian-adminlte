d5025d68ec6a7506164f952657dae73680baac22
Ricardo Tapia <rtapia92@gmail.com>
2018-01-18 17:50:52 -0600

    better view

--- addon/components/lte-navigation.js
+++ addon/components/lte-navigation.js
@@ -4,14 +4,34 @@ import layout from '../templates/components/lte-navigation';
 
 export default Component.extend({
   layout,
+  /**
+   * Empty tag name since element needs to be direct child of main node
+   */
   tagName: '',
+  /**
+   * Defines clickable route for top left logo
+   */
   logoRoute: 'index',
+  /**
+   * URI path for `img` `src` attribute
+   */
   logoUrl: null,
+  /**
+   * Left sidebar status
+   */
   collapsed: false,
+  /**
+   * User options panel open
+   */
+  userDropdownOpen: false,
+  /**
+   * Current User
+   */
   currentUser: 'John Doe',
   init(){
     this._super(...arguments);
     schedule('afterRender', ()=>{
+      // Calculates window height for canvas
       let header = document.getElementsByTagName('header')[0];
       let footer = document.getElementsByTagName('footer')[0];
 
@@ -22,9 +42,15 @@ export default Component.extend({
     });
   },
   actions: {
+    /**
+     * Calls logout function
+     */
     logout(){
       this.get('onLogout')();
     },
+    /**
+     * Toggles left sidebar status
+     */
     toggleNavigation(){
       if(!this.get('collapsed')){
         document.getElementsByTagName("body")[0].classList.add('sidebar-collapse');
@@ -33,6 +59,12 @@ export default Component.extend({
         document.getElementsByTagName("body")[0].classList.remove('sidebar-collapse');
         this.set('collapsed', !this.get('collapsed'));
       }
+    },
+    /**
+     * Toggles user panel settings view
+     */
+    openUserDropdown(){
+      this.toggleProperty('userDropdownOpen');
     },
   }
 });
