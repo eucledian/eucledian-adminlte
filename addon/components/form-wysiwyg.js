4bc4cc907c49e7aa975a9d1fe3b91bc1538197e0
Ricardo Tapia <rtapia92@gmail.com>
2018-02-19 20:23:30 -0600

    add better wysiwyg

--- addon/components/form-wysiwyg.js
+++ addon/components/form-wysiwyg.js
@@ -7,7 +7,14 @@ export default Component.extend({
   classNames: ['form-group', 'form-for-wysiwyg'],
   classNameBindings: ['propName'],
   options: computed(function(){
-    return {};
+    return {
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor textcolor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste code help wordcount'
+      ],
+      toolbar: 'insert | undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+    };
   }),
   propName: computed(function(){
     return `model-${this.get('prop')}`;
